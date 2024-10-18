194f2ba4320f0b570f220689dcab11b9b81b9127
Beatrice Gilfix <gilfixbea@gmail.com>
2024-10-18 10:40:05 -0700

    Untested "working" build for noble gas notation

--- ElectronConfig.py
+++ ElectronConfig.py
@@ -18,6 +18,7 @@ def generateElectronConfiguration(numElectrons):
         if "s" in subshell:
             if electronsInSubshell == 1:
                 unpairedElectrons += 1
+        #note to self - the first number here is the amount of electron holders
         elif "p" in subshell:
             unpairedElectrons += (3 - electronsInSubshell // 2)
         elif "d" in subshell:
@@ -33,9 +34,50 @@ def generateElectronConfiguration(numElectrons):
     
     return configuration
 
+def nobleGasCorrection(config, specifier):
+    pos = config.find(specifier)
+    if pos != -1:
+        newConfig = config[pos + len(specifier):]
+        return newConfig
+    return "err - specifier not found"
+
+#"main" area of code
 print("Type -1 to quit")
+mode = str(input("Noble gas mode? (Y/N): "))
+if mode.lower() == 'y':
+    nobleGasMode = True
+else:
+    nobleGasMode = False    
 while True:
-    numElectrons = int(input("Enter the number of electrons: "))
-    if numElectrons == -1:
-        break
-    print(generateElectronConfiguration(numElectrons))
+    if nobleGasMode:
+        numElectrons = int(input("Enter the number of electrons present in the atom: "))
+        configuration = generateElectronConfiguration(numElectrons)
+        if 2 > numElectrons:
+            print("nobleGasMode not applicable")
+        elif 10 > numElectrons > 2 :
+            nobleGas = "[He]"
+            configuration = nobleGasCorrection(configuration,"1s^2")
+        elif 18 > numElectrons >= 10:
+            nobleGas = "[Ne]"
+            configuration = nobleGasCorrection(configuration,"2p^6")
+        elif 36 > numElectrons >= 18:
+            nobleGas = "[Ar]"
+            configuration = nobleGasCorrection(configuration,"3p^6")
+        elif 54 > numElectrons >= 36:
+            nobleGas = "[Kr]"
+            configuration = nobleGasCorrection(configuration,"4p^6")
+        elif 86 > numElectrons >= 54:
+            nobleGas = "[Xe]"
+            configuration = nobleGasCorrection(configuration,"5p^6")
+        elif numElectrons > 86:
+            nobleGas = "[Rn]"
+            configuration = nobleGasCorrection(configuration,"6p^6")
+        else:
+            configuration = "error"
+            nobleGas = "error"    
+        print(nobleGas + configuration)         
+    else:    
+        numElectrons = int(input("Enter the number of electrons present in the atom: "))
+        if numElectrons == -1:
+            break
+        print(generateElectronConfiguration(numElectrons))
