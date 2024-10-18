0cb16d63882e1fd4a52cf9381a4b92d550a20712
Beatrice Gilfix <gilfixbea@gmail.com>
2024-10-18 11:10:57 -0700

    Fun fun documentation, tweaks to nobleGasCorrection() and hopefully fixed the 0 bugs found

--- ElectronConfig.py
+++ ElectronConfig.py
@@ -1,4 +1,13 @@
 def generateElectronConfiguration(numElectrons):
+    """
+    Builds standard electron configuration
+
+    Args:
+        numElectrons (int): Number of electrons in isotope
+
+    Returns:
+        String: Electron configuration
+    """
     subshells = [("1s", 2), ("2s", 2), ("2p", 6), ("3s", 2), ("3p", 6), ("4s", 2), 
                  ("3d", 10), ("4p", 6), ("5s", 2), ("4d", 10), ("5p", 6), ("6s", 2), 
                  ("4f", 14), ("5d", 10), ("6p", 6), ("7s", 2), ("5f", 14), ("6d", 10), 
@@ -34,15 +43,26 @@ def generateElectronConfiguration(numElectrons):
     
     return configuration
 
-def nobleGasCorrection(config, specifier):
+def nobleGasCorrection(config, specifier, nobleGas):
+    """
+    Takes in the electron config, the electron subshell associated with the noble gas in the numElectron range and the nobleGas being used
+    Returns the correct noble gas notation
+
+    Args:
+        config (String): Feed in the current electron configuration
+        specifier (String): Everything behind and including the specifier will be removed in the return
+        nobleGas (String): Noble gas being used in the noble gas notation electron configuration 
+    Returns:
+        String: 
+    """
     pos = config.find(specifier)
     if pos != -1:
-        newConfig = config[pos + len(specifier):]
+        newConfig = nobleGas + config[pos + len(specifier):]
         return newConfig
-    return "err - specifier not found"
+    return "Err; specifier not found"
 
 #"main" area of code
-print("Type -1 to quit")
+print("Input -1 to quit")
 mode = str(input("Noble gas mode? (Y/N): "))
 if mode.lower() == 'y':
     nobleGasMode = True
@@ -51,31 +71,35 @@ else:
 while True:
     if nobleGasMode:
         numElectrons = int(input("Enter the number of electrons present in the atom: "))
+        if numElectrons == -1:
+            break
         configuration = generateElectronConfiguration(numElectrons)
+        #This massive if elif chain handles ranges and assigns noble gas notation to the closest noble gas
         if 2 > numElectrons:
             print("nobleGasMode not applicable")
         elif 10 > numElectrons > 2 :
             nobleGas = "[He]"
-            configuration = nobleGasCorrection(configuration,"1s^2")
+            configuration = nobleGasCorrection(configuration,"1s^2",nobleGas)
         elif 18 > numElectrons >= 10:
             nobleGas = "[Ne]"
-            configuration = nobleGasCorrection(configuration,"2p^6")
+            configuration = nobleGasCorrection(configuration,"2p^6",nobleGas)
         elif 36 > numElectrons >= 18:
             nobleGas = "[Ar]"
-            configuration = nobleGasCorrection(configuration,"3p^6")
+            configuration = nobleGasCorrection(configuration,"3p^6",nobleGas)
         elif 54 > numElectrons >= 36:
             nobleGas = "[Kr]"
-            configuration = nobleGasCorrection(configuration,"4p^6")
+            configuration = nobleGasCorrection(configuration,"4p^6",nobleGas)
         elif 86 > numElectrons >= 54:
             nobleGas = "[Xe]"
-            configuration = nobleGasCorrection(configuration,"5p^6")
+            configuration = nobleGasCorrection(configuration,"5p^6",nobleGas)
         elif numElectrons > 86:
             nobleGas = "[Rn]"
-            configuration = nobleGasCorrection(configuration,"6p^6")
+            configuration = nobleGasCorrection(configuration,"6p^6",nobleGas)
         else:
-            configuration = "error"
-            nobleGas = "error"    
-        print(nobleGas + configuration)         
+            configuration = "Err; no valid electron configuration found"
+            nobleGas = ""  
+              
+        print(configuration)         
     else:    
         numElectrons = int(input("Enter the number of electrons present in the atom: "))
         if numElectrons == -1:
